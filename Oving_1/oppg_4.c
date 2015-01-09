a2ddc3726a2942085e71fd140b9a2cac7919a6cf
pfeiring <pfeiring@live.com>
2015-01-09 12:44:34 +0100

    Oppgave 4 C, debug

---
+++ Oving_1/oppg_4.c
@@ -0,0 +1,36 @@
+#include <pthread.h>
+#include <stdio.h>
+
+int i = 0;
+
+void* thread_function_1() {
+	for (int j = 0; j < 3; j++) {
+		i++;
+		printf("1: %i;", i);
+	}
+	return NULL;
+}
+
+void* thread_function_2() {
+	for (int j = 0; j < 3; j++) {
+		i--;
+		printf("2: %i", i);
+	}
+	return NULL;
+}
+
+int main() {
+
+	pthread_t thread_1;
+	pthread_t thread_2;
+
+	pthread_create(&thread_1, NULL, thread_function_1, NULL);
+	pthread_create(&thread_2, NULL, thread_function_2, NULL);
+
+	pthread_join(thread_1, NULL);
+	pthread_join(thread_2, NULL);
+
+	printf("%i\n", i);
+
+	return 0;
+}
