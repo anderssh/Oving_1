52bba8773e88b42a6580c5946c775caed1b3a181
= <=>
2015-01-21 08:13:48 +0000

    Python lock, oppgave ferdig

--- Oving_2/oppg_1.py
+++ Oving_2/oppg_1.py
@@ -1,24 +1,31 @@
 # python oppg_4.py
 
 from threading import Thread;
+from threading import Lock;
 
 i = 0;
 
-def thread_function_1():
+def thread_function_1(lock):
 	global i;
 	for j in range(0, 1000000):
+		lock.acquire();
 		i = i + 1;
+		lock.release();
 
-def thread_function_2():
+def thread_function_2(lock):
 	global i;
 	for j in range(0, 1000000):
+		lock.acquire();
 		i = i - 1;
+		lock.release();
 
 def main():
 	global i;
 
-	thread_1 = Thread(target = thread_function_1, args = ());
-	thread_2 = Thread(target = thread_function_2, args = ());
+	lock = Lock();
+
+	thread_1 = Thread(target = thread_function_1, args = (lock,));
+	thread_2 = Thread(target = thread_function_2, args = (lock,));
 
 	thread_1.start();
 	thread_2.start();
