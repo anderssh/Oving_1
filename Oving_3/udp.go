7e4cfefcb362275fa1a91fb69606c9f5ed7fd424
pfeiring <pfeiring@live.com>
2015-01-28 12:31:53 +0100

    UDP more bug fixes

--- Oving_3/udp.go
+++ Oving_3/udp.go
@@ -17,7 +17,7 @@ func receive(conn *net.UDPConn) {
 	}
 }
 
-func transmit(conn *net.UDPConn) {
+func listen(conn *net.UDPConn) {
 	
 	for {
 		time.Sleep(2000*time.Millisecond);
@@ -30,23 +30,20 @@ func transmit(conn *net.UDPConn) {
 
 func main() {
 
-	broadcastIP := "129.241.187.255";
-	broadcastPort := 30000;
-	broadcastAddr, _ := net.ResolveUDPAddr("udp", broadcastIP + ":" + strconv.Itoa(broadcastPort));
-
 	serverIP := "129.241.187.255";
 	serverPort := 20016;
 	serverAddr, _ := net.ResolveUDPAddr("udp", serverIP + ":" + strconv.Itoa(serverPort));
 
-	localAddr, _ := net.ResolveUDPAddr("udp", ":" + strconv(serverPort));
+	listenPort := 20016;
+	listenAddr, _ := net.ResolveUDPAddr("udp", ":" + strconv.Itoa(serverPort));
 
 	fmt.Println(localAddr);
 	fmt.Println(serverAddr);
 
-	listenConn, _ := net.ListenUDP("udp", localAddr);
+	listenConn, _ := net.ListenUDP("udp", listenAddr);
 	transmitConn, _ := net.DialUDP("udp", nil, serverAddr);
 
-	go receive(listenConn);
+	go listen(listenConn);
 	go transmit(transmitConn);
 
 	d_chan := make(chan bool, 1);
