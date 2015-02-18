8f1d5a2eaca528417e126d2e09db966ea45d6c10
pfeiring <pfeiring@live.com>
2015-02-18 11:27:30 +0100

    Alpha YO 1.0

--- Oving_4/main.go
+++ Oving_4/main.go
@@ -4,17 +4,141 @@ import(
 	"os"
 	"fmt"
 	"os/exec"
-)
+	"time"
+	"net"
+	"encoding/binary"
+);
 
+//-----------------------------------------------//
 
-func  backup(){
-	//listen
+func listen(listenConnection *net.UDPConn, listenChannel chan int) {
+	
+	messageBuffer := make([]byte, 1024);
+
+	for {
+		messageLength, _, _ := listenConnection.ReadFromUDP(messageBuffer);
+		
+		data := binary.BigEndian.Uint64(messageBuffer[0:messageLength]);
+
+		listenChannel <- int(data);
+	}
 }
 
-func master(){
-	//print
-	//send
+func timeout(timeoutChannel chan bool, resetTimer chan bool) {
+
+	start := time.Now();
+
+	for {
+		select {
+			case <-resetTimer:
+				start = time.Now();
+			default:
+				if time.Since(start) >= time.Millisecond*500 {
+					timeoutChannel <- true;
+				}
+		}
+	}
 }
+
+func backup(){
+
+	fmt.Println("Creating backup...");
+
+	i := 0;
+
+	listenAddress, _ := net.ResolveUDPAddr("udp", "localhost:20005");
+	listenConnection, _ := net.ListenUDP("udp", listenAddress);
+
+	listenChannel := make(chan int);
+	timeoutChannel := make(chan bool);
+	resetTimer := make(chan bool);
+
+	go listen(listenConnection, listenChannel);
+	go timeout(timeoutChannel, resetTimer);
+
+	for {
+		select {
+			case message := <- listenChannel:
+				
+				if message == -1{
+					resetTimer <- true;
+				} else {
+					i = message;
+				}
+
+			case <- timeoutChannel:
+
+				listenConnection.Close()
+
+				close(listenChannel);
+				close(timeoutChannel);
+				close(resetTimer);
+
+				go master(i);
+		}
+	}
+}
+
+//-----------------------------------------------//
+
+func counting(i int, sendChannel chan int) {
+	
+	for {
+		fmt.Println(i);
+		sendChannel <- i;
+		i++;
+		time.Sleep(100*time.Millisecond)
+
+	}
+}
+	
+func notify(sendChannel chan int) {
+	
+	for {
+		time.Sleep(20*time.Millisecond)
+		sendChannel <- -1; 
+	}
+}
+
+func send(sendChannel chan int) {
+	
+	for {
+		message := <- sendChannel;
+
+		sendAddress, _ := net.ResolveUDPAddr("udp", "localhost:20005");
+		sendConnection, _ := net.DialUDP("udp", nil, sendAddress);
+
+		bs := make([]byte, 4)
+    	binary.LittleEndian.PutUint32(bs, uint32(message))
+
+		sendConnection.Write(bs);
+	}
+}
+
+
+func master(i int) {
+
+	fmt.Println("Creating master...");
+	
+	command := exec.Command("gnome-terminal", "-e", "go run main.go");
+
+	err := command.Run();
+	if err != nil {
+		fmt.Println(err);
+		panic("Error in creating backup");
+	}
+
+	sendChannel := make(chan int);
+
+	go send(sendChannel);
+	go counting(i, sendChannel);
+	go notify(sendChannel);
+
+	deadChannel := make(chan bool)
+	<- deadChannel;
+}
+
+//-----------------------------------------------//
 
 func main() {
 	
@@ -26,16 +150,11 @@ func main() {
 	}
 
 	if role == "master" {
-		//make backup
-		fmt.Println("asd");
-		command := exec.Command("gnome-terminal", "-e", "");
-		fmt.Println(command);
-		err := command.Run();
-		fmt.Println("Ferdig", err);
-		//go master();
+		go master(0);
 	} else {
-		//go backup();
+		go backup();
 	}
-	
-	
+
+	deadChannel := make(chan bool)
+	<- deadChannel;
 }
